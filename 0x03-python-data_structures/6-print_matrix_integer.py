0055f88accb2dcffd09f84b4c49d30b95b0ce9dc
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-01-07 11:18:01 -0800

    52

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix), 1):
-        for j in range(0, len(matrix[i]), 1):
-            print(matrix[j])
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" ")
+        print()
