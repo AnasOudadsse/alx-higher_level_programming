fd22599451c3756eba5e1866b1a72c1c63fb6bde
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-01-12 13:51:15 -0800

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     newDict = {}
 
-    newDict = {key : value * 2 for key, value in a_dictionary.items()}
+    newDict = {key: value * 2 for key, value in a_dictionary.items()}
     return newDict
