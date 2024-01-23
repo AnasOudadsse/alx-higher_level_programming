f23a5a3b4ddfaac3c501cdad6ac1d70d0046fa52
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-01-23 11:48:40 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""square module"""
+
 class Square:
+    """defining the Square class"""
     def __init__(self, size):
+    """constructor
+
+    Attributes:
+        size: length of the square 
+    """
         self.__size = size
