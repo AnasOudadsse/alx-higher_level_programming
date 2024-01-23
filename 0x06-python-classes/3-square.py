cc9d4a82f90a21f89139c55db0be1cce10705f53
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-01-23 12:26:36 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """defining Square Class"""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not int
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square.
+
+        Returns:
+            the size squared
+        """
+        return self.__size ** 2
