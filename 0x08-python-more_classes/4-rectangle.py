850f56b262e7e6dbe77356d3a4a4016f36df98aa
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-01-31 13:56:14 -0800

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -58,6 +58,13 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return("")
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
 
         rect = []
         for i in range(self.__height):
