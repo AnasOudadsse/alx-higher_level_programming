7667bb115d40b0f888e4be13809f0cb9e27b3bd5
AnasOudadsse <anas.oudadsse1@gmail.com>
2024-02-06 13:38:45 -0800

    task 7&8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""defines a func that returns the __dict__ of an obj"""
+"""Defines a Python class-to-JSON function."""
+
 
 def class_to_json(obj):
-    """returns the dictionary description of an obj"""
+    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
